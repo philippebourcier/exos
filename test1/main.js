da1e761fb4571d1258f901c2708641ec14ba2399
philippebourcier <none>
2021-03-28 11:23:30 +0000

    test1

---
+++ test1/main.js
@@ -0,0 +1,26 @@
+
+function update_buttons() {
+	$.getJSON("buttons.json",function(data) {
+		$.each(data,function(key,vtab) {
+			// console.log(vtab);
+			$("#"+key).text(vtab.content+" ("+vtab.clicks+")");
+		});
+	});
+}
+
+function init() {
+
+update_buttons();
+
+// SOME JQUERY CODE FOR THE BUTTONS HERE...
+
+// BUTTON1 WHEN CLICKED WILL BECOME : Voici le Bouton 1 (1)
+// BUTTON1 WHEN CLICKED AGAIN WILL BECOME : Voici le Bouton 1 (2)
+// AND SO ON... ie: BUTTON1 WHEN CLICKED number of clicks TIMES WILL BECOME : Voici le Bouton 1 (number of clicks)
+
+// BUTTON2 WHEN CLICKED WILL BECOME : Et ceci est le Bouton 2 (1)
+// BUTTON2 WHEN CLICKED AGAIN WILL BECOME : Et ceci est le Bouton 2 (2)
+// AND SO ON... ie: BUTTON2 WHEN CLICKED number of clicks TIMES WILL BECOME : Et ceci est le Bouton 2 (number of clicks)
+
+}
+
